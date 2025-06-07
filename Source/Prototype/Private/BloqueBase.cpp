8873bc1eeea8a36b6946ea4b5a3755a49d5f122d
MicaMirandaC <mirandacoroangelamicaela@gmail.com>
2025-06-07 01:41:25 -0400

    Prototype

--- Source/Prototype/Private/BloqueBase.cpp
+++ Source/Prototype/Private/BloqueBase.cpp
@@ -6,7 +6,7 @@
 // Sets default values
 ABloqueBase::ABloqueBase()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	
 	PrimaryActorTick.bCanEverTick = true;
 
 	//Representa la malla del bloque
